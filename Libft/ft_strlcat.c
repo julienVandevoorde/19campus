1b7baee001a6b5fc39f7dc077f64f9316be20950
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2025-04-25 16:37:33 +0200

    strlcat done

---
+++ Libft/ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvandevo <jvandevo@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 14:51:29 by jvandevo          #+#    #+#             */
+/*   Updated: 2025/04/25 16:31:31 by jvandevo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t  ft_strlcat(char *dest, const char *src, size_t size)
+{
+    size_t  i;
+    size_t  dest_len;
+    size_t  src_len;
+
+    dest_len = ft_strlen(dest);
+    src_len = ft_strlen(src);
+    
+    if (dest_len >= size)
+        return (size + src_len);
+    
+    i = 0;
+    while (src[i] && (dest_len + i) < (size - 1))
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    
+    dest[dest_len + i] = '\0';
+    return (dest_len + src_len);
+}
