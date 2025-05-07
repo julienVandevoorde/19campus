c4f3fb1ebc4558c38a382cd96eeacee280f561ca
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2025-05-07 15:21:08 +0200

    ft_memchr done

--- Libft/ft_memchr.c
+++ Libft/ft_memchr.c
@@ -6,7 +6,25 @@
 /*   By: jvandevo <jvandevo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:14:34 by jvandevo          #+#    #+#             */
-/*   Updated: 2025/05/07 15:14:35 by jvandevo         ###   ########.fr       */
+/*   Updated: 2025/05/07 15:18:15 by jvandevo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    size_t i;
+    const unsigned char *ptr;
+
+    ptr = (const unsigned char *)s;
+    i = 0;
+
+    while (i < n)
+    {
+        if (ptr[i] == (unsigned char)c)
+            return ((void *)(ptr + i));
+        i++;
+    }
+    return (NULL);
+}
