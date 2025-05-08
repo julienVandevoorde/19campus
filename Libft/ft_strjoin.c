c0dbb146ff7301e70c607dc51730c968d600791d
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2025-05-08 14:26:44 +0200

    ft_strjoin done

---
+++ Libft/ft_strjoin.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvandevo <jvandevo@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 14:11:51 by jvandevo          #+#    #+#             */
+/*   Updated: 2025/05/08 14:26:32 by jvandevo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int ft_strlen(const char *str){
+    unsigned int i;
+    i = 0;
+    while(str[i]){
+        i++;
+    }
+    return i;
+}
+
+char *ft_strjoin(const char *s1, const char *s2){
+    unsigned int i;
+    unsigned int j;
+    unsigned int k;
+    char *dest;
+
+    i = 0;
+    j = 0;
+    k = 0;
+    dest = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+    if(!dest){
+        return NULL;
+    }
+
+    while(s1[i]){
+        dest[k] = s1[i];
+        i++;
+        k++;
+    }
+    while(s2[j]){
+        dest[k] = s2[j];
+        j++;
+        k++;
+    }
+
+    dest[k] = '\0';
+    return dest;
+}
