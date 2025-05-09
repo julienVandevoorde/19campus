e2d46749130945d7542e31b6642785f478f3b5fe
Julien Vandevoorde <julienvandevoorde00@gmail.com>
2025-05-09 17:08:14 +0200

    norme + correction de tout, enfin mon dieu j'en peux plus

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,27 +6,49 @@
 /*   By: jvandevo <jvandevo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 11:19:35 by jvandevo          #+#    #+#             */
-/*   Updated: 2025/05/08 11:38:02 by jvandevo         ###   ########.fr       */
+/*   Updated: 2025/05/09 16:56:24 by jvandevo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(const char *str){
-    unsigned int i = 0;
-    int sign = 1;
-    int nb = 0;
+#include "libft.h"
 
-    while(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)){
+int ft_atoi(const char *str)
+{
+    int i;
+    int sign;
+    int result;
+
+    i = 0;
+    sign = 1;
+    result = 0;
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign *= -1;
         i++;
     }
-    if(str[i] == '+' || str[i] == '-'){
-        if(str[i] == '-'){
-            sign = -sign;
-        }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
         i++;
     }
-    while(str[i] >= '0' && str[i] <= '9'){
-        nb = nb * 10 + (str[i] - '0');
-        i++;
-    }
-    return (sign*nb);
+    return (result * sign);
 }
+
+/*#include <stdio.h>
+int	main(int argc, char** argv)
+{
+    int	result;
+
+    if (argc != 2)
+    {
+        printf("Please put only one string");
+        return (0);
+    }
+    result = ft_atoi(argv[1]);
+    int result_a = atoi(argv[1]);
+    printf("myResult = %d, atoi: %d", result, result_a);
+    return (0);
+}*/
